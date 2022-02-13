3fd3fa569cbe9d854ed77f88062268fd3470b41d
Bank <deknan70@gmail.com>
2022-02-13 23:02:06 +0700

    start point

---
+++ src/classes/UnitImp.java
@@ -0,0 +1,29 @@
+package classes;
+import interfaces.Unit;
+
+public class UnitImp implements Unit {
+    public void move()
+    {
+
+    }
+
+    public int isVirus()
+    {
+        return 0;
+    }
+
+    public int isAntiBody()
+    {
+        return 0;
+    }
+
+    public void attack()
+    {
+
+    }
+
+    public int[] status()
+    {
+        return null;
+    }
+}
---
+++ src/interfaces/Unit.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface Unit {
+    void move();
+    void attack();
+    int isVirus();
+    int isAntiBody();
+    int[] status();
+}
