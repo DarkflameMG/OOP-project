ee0db84845424910ee013a74a51b673a63e23f66
ploylapas <lapasrada_k@cmu.ac.th>
2022-02-14 20:48:06 +0700

    add document of Tokenizer interface

--- src/interfaces/Tokenizer.java
+++ src/interfaces/Tokenizer.java
@@ -1,5 +1,20 @@
 package interfaces;
+
+import classes.TokenizerError;
+
 public interface Tokenizer {
+
+    /** 
+     * Get the next token  
+     * @return next token
+    */
     String peek();
-    String consume();
+
+    /**
+     * Get the current token
+     * @return current token
+     * @throws TokenizerError if unknown token has been found
+     */
+    String consume() throws TokenizerError;
+    
 }
