dabbacae3de03bd9930838e008b4056d376b20a9
ploylapas <lapasrada_k@cmu.ac.th>
2022-02-14 22:55:29 +0700

    try coding executeNodeStatement

---
+++ src/classes/NodeIfStatement.java
@@ -0,0 +1,26 @@
+package classes;
+
+import interfaces.Node;
+import interfaces.Execute;
+
+
+public class NodeIfStatement implements Execute{
+    Execute statementTrue;
+    Execute statementFalse;
+    Node expr;
+
+    public NodeIfStatement (Execute statementTrue, Execute statementFalse, Node expr){
+        this.statementTrue = statementTrue;
+        this.statementFalse = statementFalse;
+        this.expr = expr;
+    }
+    
+    @Override
+    public void execute() {
+        if(expr.evaluate()<=0){
+            statementFalse.execute();
+        }else if(expr.evaluate()>0){
+            statementTrue.execute();
+        }
+    }   
+}
---
+++ src/classes/NodeProgram.java
@@ -0,0 +1,13 @@
+package classes;
+
+import interfaces.Execute;
+
+public class NodeProgram implements Execute{
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
