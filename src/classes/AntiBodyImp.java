7f5abf392108d234d5bdd13feffb3b1e4c5c07d5
Bank <deknan70@gmail.com>
2022-02-14 15:59:26 +0700

    add VirusUnit interface and AntiBody class

---
+++ src/classes/AntiBodyImp.java
@@ -0,0 +1,20 @@
+package classes;
+
+import interfaces.AntiBodyUnit;
+
+public class AntiBodyImp extends UnitImp implements AntiBodyUnit {
+    public AntiBodyImp()
+    {
+        super("antibody");
+    }
+
+    public void transform()
+    {
+
+    }
+
+    public void recovery()
+    {
+
+    }
+}
---
+++ src/classes/VirusImp.java
@@ -0,0 +1,16 @@
+package classes;
+
+import interfaces.VirusUnit;
+
+public class VirusImp extends UnitImp implements VirusUnit{
+
+    public VirusImp()
+    {
+        super("virus");
+    }
+
+    public void spawn()
+    {
+
+    }
+}
