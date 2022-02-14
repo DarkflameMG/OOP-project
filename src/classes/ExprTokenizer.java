00bf392b8f7005c97e388e46cf536f6d8343910e
ploylapas <lapasrada_k@cmu.ac.th>
2022-02-14 22:00:23 +0700

    coding computeNext,initial,peek,consume

--- src/classes/ExprTokenizer.java
+++ src/classes/ExprTokenizer.java
@@ -2,32 +2,65 @@ package classes;
 
 import interfaces.Tokenizer;
 
-public class ExprTokenizer implements Tokenizer {
-    // private String src;
-    // private String next;
-    // private int pos;
+public class ExprTokenizer implements  Tokenizer{
+    private static ExprTokenizer instance;
+    private String src;
+    private String next;
+    private int pos;
 
-    public ExprTokenizer(String src){
-        // this.src = src;
-        // pos = 0;
-        // computeNext();
+    public static ExprTokenizer getInstance(){
+        ExprTokenizer.instance = new ExprTokenizer();
+        return ExprTokenizer.instance;
     }
 
-    private void computeNext(){
+    public void initial(String src) throws TokenizerError {
+        this.src = src;
+        pos = 0;
+        computeNext();
+    }
 
+    private void computeNext() throws TokenizerError{
+        StringBuilder s = new StringBuilder();
+        while (pos < src.length() && Character.isWhitespace(src.charAt(pos))) pos++;  // ignore whitespace
+        if (pos < src.length()) {
+            char c = src.charAt(pos);
+            if (Character.isDigit(c)) {  // start of number
+                s.append(c);
+                for (pos++; pos < src.length() &&
+                        Character.isDigit(src.charAt(pos)); pos++)
+                    s.append(src.charAt(pos));
+            } else if (Character.isLetter(c)) {  // start of string
+                s.append(c);
+                pos++;
+            }
+            else if (c == '+' || c == '(' || c == ')' || c == '*' || c == '/' || c == '%') {
+                s.append(c);
+                pos++;
+            } else throw new TokenizerError("Unknown: " + c);
+        }
+        next = s.toString();
+    }
+
+    public boolean peek(String s){
+        if(!src.isEmpty()) return peek().equals(s);
+        return false;
+    }
+
+    public void consume(String s) throws TokenizerError {
+        if(peek(s)) {
+            consume();
+        }else
+            throw new TokenizerError("Unknown: " + s);
     }
 
     @Override
-    public String peek() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    public String peek() { return next; }
 
     @Override
-    public String consume() {
-        // TODO Auto-generated method stub
-        return null;
+    public String consume() throws TokenizerError {
+        String result = next;
+        computeNext();
+        return result;
     }
-    
-    
+
 }
