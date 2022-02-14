4d1cd125db19d2a16c9b657d16125e6847e421e1
Bank <deknan70@gmail.com>
2022-02-14 16:02:16 +0700

    Unit class add method get status

--- src/classes/UnitImp.java
+++ src/classes/UnitImp.java
@@ -2,6 +2,16 @@ package classes;
 import interfaces.Unit;
 
 public class UnitImp implements Unit {
+    protected int hp = 0;
+    protected int damage = 0;
+    protected int defence = 0;
+    protected String species;
+
+    protected UnitImp(String species)
+    {
+        this.species = species;
+    }
+
     public void move()
     {
 
@@ -9,21 +19,43 @@ public class UnitImp implements Unit {
 
     public int isVirus()
     {
+        if(species.equals("virus"))
+            return 1;
         return 0;
     }
 
     public int isAntiBody()
     {
+        if(species.equals("antibody"))
+            return 1;
         return 0;
     }
 
-    public void attack()
+    public void attack(Unit target)
     {
-
+        target.decreaseHp(damage);
     }
 
-    public int[] status()
+    public void decreaseHp(int damage)
     {
-        return null;
+        if (this.hp - damage >= 0)
+            this.hp -= damage;
+        else
+            this.hp = 0;
+    }
+
+    public int getDamage()
+    {
+        return damage;
+    }
+
+    public int getHp()
+    {
+        return hp;
+    }
+
+    public int getDefence()
+    {
+        return defence;
     }
 }
