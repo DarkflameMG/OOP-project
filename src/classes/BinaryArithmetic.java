f3dc297d4ea8af7e12ce31e82fbfb000fb84239b
ploylapas <lapasrada_k@cmu.ac.th>
2022-02-15 14:42:25 +0700

    almost complete-BinayArithmetic

--- src/classes/BinaryArithmetic.java
+++ src/classes/BinaryArithmetic.java
@@ -1,5 +1,30 @@
 package classes;
 
-public class BinaryArithmetic {
+import interfaces.Node;
+
+public class BinaryArithmetic implements Node{
+    Node left;
+    Node right;
+    String opr;
+
+    public BinaryArithmetic(Node left, Node right, String opr){
+        this.left = left;
+        this.right = right;
+        this.opr = opr;
+    }
     
+    @Override
+    public double evaluate() throws SyntaxError{
+        double a = left.evaluate();
+        double b = right.evaluate();
+
+        if(opr.equals("+")) return a+b;
+        if(opr.equals("-")) return a-b;
+        if(opr.equals("*")) return a*b;
+        if(opr.equals("/")) return a/b;
+        if(opr.equals("%")) return a%b;
+        if(opr.equals("^")) return Math.pow(a,b);
+        // return 0;
+        throw new SyntaxError("unknown oprator: "+ opr);
+    }
 }
