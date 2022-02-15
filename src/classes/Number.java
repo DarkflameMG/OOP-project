23fec70ed4cd00fafeb368409184ad5abe434329
ploylapas <lapasrada_k@cmu.ac.th>
2022-02-15 16:08:46 +0700

    coding Number class

--- src/classes/Number.java
+++ src/classes/Number.java
@@ -1,5 +1,16 @@
 package classes;
 
-public class Number {
-    
+import interfaces.Node;
+
+public class Number implements Node{
+    int num;
+
+    public Number(int num){
+        this.num = num;
+    }
+
+    @Override
+    public int evaluate() throws SyntaxError {
+        return num;
+    }
 }
