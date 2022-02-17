26e779d335af6f563ca12c6500b271c4f988412a
Bank <deknan70@gmail.com>
2022-02-17 23:53:45 +0700

    add class Variable

---
+++ src/classes/parse/Variable.java
@@ -0,0 +1,22 @@
+package classes.parse;
+
+import interfaces.Node;
+
+import java.util.Map;
+
+public class Variable implements Node {
+    private String name;
+
+    public Variable(String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public int evaluate(Map<String,Node> bindings)throws SyntaxError
+    {
+        if (bindings.containsKey(name))
+            return bindings.get(name).evaluate(bindings);
+        throw new SyntaxError("undefined variable: " + name);
+    }
+}
