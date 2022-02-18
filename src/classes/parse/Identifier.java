7cbc23d8681ab981944108bcf782bcbb289dc209
Bank <deknan70@gmail.com>
2022-02-18 20:26:17 +0700

    modeify Indentifier class

--- src/classes/parse/Identifier.java
+++ src/classes/parse/Identifier.java
@@ -5,11 +5,18 @@ import interfaces.Node;
 import java.util.Map;
 
 public class Identifier implements Node{        //parsePower
+    private String name;
+
+    public Identifier(String name)
+    {
+        this.name = name;
+    }
 
     @Override
     public int evaluate(Map<String,Node> bindings) throws SyntaxError {
-        // TODO Auto-generated method stub
-        return 0;
+        if (bindings.containsKey(name))
+            return bindings.get(name).evaluate(bindings);
+        throw new SyntaxError("undefined variable: " + name);
     }
     
 }
