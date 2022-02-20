e60352846cfe368d79cb3375c6c63108279994bb
winnapat <GGgun2013@gmail.com>
2022-02-20 21:17:00 +0700

    Change font and create modal for pause button

--- frontend/carin-v2/src/component/Maingame.tsx
+++ frontend/carin-v2/src/component/Maingame.tsx
@@ -3,8 +3,18 @@ import '../style/style-maingame.css';
 import antibodyA from '../image/antibodyA.png';
 import antibodyB from '../image/antibodyB.png';
 import antibodyC from '../image/antibodyC.png';
+import closeBT from '../image/CloseBTmini.png'
+import { useState } from 'react';
 
 const Maingame = () => {
+    const [open, setOpen] = useState(false);
+    const handleOpen = ()=>{
+        setOpen(true);
+    }
+    const handleClose = ()=>{
+        setOpen(false);
+    }
+    
     function clickSpeedUP() {
         console.log("speed up clicked")
     }
@@ -22,17 +32,17 @@ const Maingame = () => {
             {/* navbar */}
             <div className="topnav" >
                 <div className="topnav-centered">
-                    <p className="active">Wave 1/5</p>
+                    <p className="active font-Righteous">Wave 1/5</p>
                 </div>
 
                 <div >
-                    <p >Money $100000</p>
+                    <p className="font-Righteous">Money $100000</p>
                 </div>
 
                 <div className="topnav-right">
-                    <button onClick={clickSpeedUP}>Speed up</button>
-                    <button onClick={clickSpeedDown}>Speed down</button>
-                    <button onClick={clickPause} className='bg-pause'>Pause</button>
+                    <button onClick={clickSpeedUP} className="font-Righteous">Speed up</button>
+                    <button onClick={clickSpeedDown} className="font-Righteous">Speed down</button>
+                    <button onClick={()=> {clickPause(); handleOpen();}} className='bg-pause font-Righteous'>Pause</button>
                 </div>
             </div>
 
@@ -40,21 +50,65 @@ const Maingame = () => {
             {/* shop */}
 
             <div className='shop'>
-                <p className="shop-text">Shop</p>
-                <li><button className='antibody-set'>antibody A</button>
+                <p className="shop-text font-Righteous">Shop</p>
+                <li><button className='antibody-set font-Righteous'>antibody A</button>
                 <img src={antibodyA} height={200} width={200} />
                 </li>
 
-                <li><button className='antibody-set'>antibody B</button>
+                <li><button className='antibody-set font-Righteous'>antibody B</button>
                 <img src={antibodyB} height={200} width={200} />
                 </li>
 
-                <li><button className='antibody-set'>antibody C</button>
+                <li><button className='antibody-set font-Righteous'>antibody C</button>
                 <img src={antibodyC} height={200} width={200} />
                 </li>
 
             </div>
+            
+            {/**Modal */}
+            {open ? (
+        <>
+          <div
+            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          >
+            <div className="relative w-auto my-6 mx-auto max-w-3xl flex flex-col">
+                <div className='flex justify-end'>
+                    <button onClick={handleClose}>
+                        <img src={closeBT}></img>
+                    </button>
+                </div>
+                <div className='flex'>
+                    {/*content*/}
+                    <div className="border-4 border-black rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                        {/*header*/}
+                        <h3 className="text-3xl p-2 text-center font-semibold font-Righteous">
+                            PAUSE
+                        </h3>
+                        {/*body*/}
+                        <div className="flex flex-col px-6 ">
+                            <button className="font-Righteous" onClick={handleClose}>Resume</button>
+                            <button className="font-Righteous">
+                                <Link to='/'>
+                                    Quit
+                                </Link>
+                            </button>
+                            
+                        </div>    
+                    </div>
+                    <div className=''>
+                        <p className='text-transparent px-4'>S</p>
+                    </div>
+                </div>
+            </div>
+          </div>
+          {/*set bg more white */}
+          <div className="opacity-30 fixed inset-0 z-40 bg-white"></div>
+        </>
+      ) : null}
         </div>
+
+        
+
     )
 }
 
--- frontend/carin-v2/src/component/Mainmemu.tsx
+++ frontend/carin-v2/src/component/Mainmemu.tsx
@@ -4,6 +4,7 @@ import startBT from '../image/startButton.png'
 import virus from '../image/virusLogo.png'
 import credit from '../image/CreditButton.png'
 import closeBT from '../image/CloseBTmini.png'
+import '../style/style-font.css'
 import { useState } from 'react';
 
 const Mainmenu = () =>{
@@ -17,9 +18,9 @@ const Mainmenu = () =>{
     return(
         <div>
             <div className='mx-auto max-w-4xl'> 
-            <div className='flex justify-center py-10 '>
-                <img src={logo}></img>
-            </div>
+                <div className='flex justify-center py-10 '>
+                    <img src={logo}></img>
+                </div>
                 <div className='flex justify-around space-x-10'>
                     <img src={virus}></img>
                     <Link to='/Maingame'>
@@ -28,54 +29,51 @@ const Mainmenu = () =>{
                     <img src={virus}></img>
                 </div>
             </div>
-            <div  className='flex justify-end py-5 px-10'>
+            <div  className='flex justify-end py-5 px-10 mx-auto'>
                 <button onClick={handleOpen}>
                     <img src={credit}></img>
                 </button>
             </div>
             {open ? (
-        <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl flex flex-col">
-                <div className='flex justify-end'>
-                    <button onClick={handleClose}>
-                        <img src={closeBT}></img>
-                    </button>
-                </div>
-                <div className='flex'>
-                    {/*content*/}
-                    <div className="border-4 border-black rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                        {/*header*/}
-                        <h3 className="text-3xl p-2 text-center font-semibold">
-                            Credit
-                        </h3>
-                        {/*body*/}
-                        <div className="relative px-6 flex-auto">
-                            <p className="my-4 text-black text-xl font-semibold ">
-                                1.khanitin Saenkan (Bank) 630610718
-                            </p>
-                            <p className="my-4 text-black text-xl font-semibold ">
-                                2.Lapasrada Kumjulla (Ploy) 630610757
-                            </p>
-                            <p className="my-4 text-black text-xl font-semibold ">
-                                3.Winnapat Kaewmora (Game) 630610761
-                            </p>
-                        </div>    
+                <>
+                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                    <div className="relative w-auto my-6 mx-auto max-w-3xl flex flex-col">
+                        <div className='flex justify-end'>
+                            <button onClick={handleClose}>
+                                <img src={closeBT}></img>
+                            </button>
+                        </div>
+                        <div className='flex'>
+                            {/*content*/}
+                            <div className="border-4 border-black rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                {/*header*/}
+                                <h3 className="text-3xl p-2 text-center font-semibold font-Righteous">
+                                    Credit
+                                </h3>
+                                {/*body*/}
+                                <div className="relative px-6 flex-auto flex-col">
+                                    <p className="my-4 text-black text-xl font-semibold font-Righteous">
+                                        1.khanitin Saenkan (Bank) 630610718
+                                    </p>
+                                    <p className="my-4 text-black text-xl font-semibold font-Righteous">
+                                        2.Lapasrada Kumjulla (Ploy) 630610757
+                                    </p>
+                                    <p className="my-4 text-black text-xl font-semibold font-Righteous">
+                                        3.Winnapat Kaewmora (Game) 630610761
+                                    </p>
+                                </div>    
+                            </div>
+                            <div className=''>
+                                <p className='text-transparent px-4'>S</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className=''>
-                        <p className='text-transparent px-3'>S</p>
-                    </div>
                 </div>
-            </div>
-          </div>
-          {/*set bg more white */}
-          <div className="opacity-30 fixed inset-0 z-40 bg-white"></div>
-        </>
-      ) : null}
-        </div>
-        
+                {/*set bg more white */}
+                <div className="opacity-30 fixed inset-0 z-40 bg-white"></div>
+                </>
+            ) : null}
+        </div>    
     )
 }
 
