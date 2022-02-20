a9084f180f6bd8b5c94e8ac6e7fc945df6353df1
winnapat <GGgun2013@gmail.com>
2022-02-20 16:34:02 +0700

    change location from index.tsx to App.t

--- frontend/carin-v2/src/App.tsx
+++ frontend/carin-v2/src/App.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import {BrowserRouter, BrowserRouter as Router,Link, Route, Routes} from 'react-router-dom'
 import Maingame from './component/Maingame';
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <Maingame/>
-      <Routes>
-        <Route path='/main'>
-          <p>main</p>
-        </Route>
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <div>
+          <h1> Home Page</h1>
+          <Link to='/Maingame'>
+            <button className='start-button'>Start</button>
+          </Link>
+        </div>
+
+        <Routes>
+          <Route path='/Maingame' element={<Maingame/>}/>
+        </Routes>
+      </div>
+    </Router>
     
   );
 }
--- frontend/carin-v2/src/index.tsx
+++ frontend/carin-v2/src/index.tsx
@@ -8,25 +8,10 @@ import { Link } from 'react-router-dom';
 import Maingame from './component/Maingame';
 
 ReactDOM.render(
-  <Router>
-    <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/Maingame' element={<Maingame/>}/>
-    </Routes>
-  </Router>,
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-function Home(){
-  return(
-    <div className='bg-gray-600 w-full h-full'>
-      <div>
-        <h1> Home Page</h1>
-        <Link to='/Maingame'>
-          <button className='start-button'>Start</button>
-        </Link>
-      </div>
-    </div>
-  )
-}
 reportWebVitals();
