fe07300118a44eb675c2b19576756b2966462aa7
Bank <deknan70@gmail.com>
2022-02-21 20:47:48 +0700

    override attack method

--- src/classes/AntiBodyImp.java
+++ src/classes/AntiBodyImp.java
@@ -1,13 +1,17 @@
 package classes;
 
 import interfaces.AntiBodyUnit;
+import interfaces.Unit;
 
 public class AntiBodyImp extends UnitImp implements AntiBodyUnit {
-    public AntiBodyImp()
+    private final int hp_gain = ConfigImp.getAntibody_gain();
+
+    public AntiBodyImp(String species)
     {
-        super("antibody",0,0);
+        super("antibody",species,0,0);
     }
 
+    @Override
     public void transform()
     {
 
@@ -15,6 +19,30 @@ public class AntiBodyImp extends UnitImp implements AntiBodyUnit {
 
     public void recovery()
     {
+        hp += hp_gain;
+    }
 
+    @Override
+    public void attack(int direction)
+    {
+        FieldImp field = FieldImp.getInstance(10,10);
+        int x = coordinateX,y = coordinateY;
+        if(direction == 1 || direction == 2 || direction == 8)
+            y += 1;
+        if(direction == 4 || direction == 5 || direction == 6)
+            y -= 1;
+        if(direction == 2 || direction == 3 || direction == 4)
+            x += 1;
+        if(direction == 6 || direction == 7 || direction == 8)
+            x -= 1;
+        Unit target = field.getTarget(x,y);
+        if(target != null)
+        {
+            target.decreaseHp(damage);
+            if(target.getHp() == 0)
+            {
+                recovery();
+            }
+        }
     }
 }
--- src/classes/VirusImp.java
+++ src/classes/VirusImp.java
@@ -1,16 +1,46 @@
 package classes;
 
+import interfaces.Unit;
 import interfaces.VirusUnit;
 
 public class VirusImp extends UnitImp implements VirusUnit{
+    private final int hp_drain = ConfigImp.getVirus_gain();
 
-    public VirusImp()
+    public VirusImp(String species)
     {
-        super("virus",0,0);
+        super("virus",species,0,0);
     }
 
     public void spawn()
     {
 
+    }
+
+    public void drain()
+    {
+        hp += hp_drain;
+    }
+
+    @Override
+    public void attack(int direction)
+    {
+        FieldImp field = FieldImp.getInstance(10,10);
+        int x = coordinateX,y = coordinateY;
+        if(direction == 1 || direction == 2 || direction == 8)
+            y += 1;
+        if(direction == 4 || direction == 5 || direction == 6)
+            y -= 1;
+        if(direction == 2 || direction == 3 || direction == 4)
+            x += 1;
+        if(direction == 6 || direction == 7 || direction == 8)
+            x -= 1;
+        Unit target = field.getTarget(x,y);
+        if(target != null)
+        {
+            target.decreaseHp(damage);
+            drain();
+            if (target.getHp() == 0)
+                target.transform();
+        }
     }
 }
