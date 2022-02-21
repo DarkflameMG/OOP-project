f8a7e2b17bb2b7a685efede105cabcbb39155213
Bank <deknan70@gmail.com>
2022-02-21 20:54:26 +0700

    organize folder

--- carinProject/src/main/java/com/carin/carinProject/classes/VirusImp.java
+++ carinProject/src/main/java/com/carin/carinProject/classes/VirusImp.java
@@ -13,4 +13,26 @@ public class VirusImp extends UnitImp implements VirusUnit {
     {
 
     }
+    @Override
+    public void attack(int direction)
+    {
+        FieldImp field = FieldImp.getInstance(10,10);
+        int x = coordinateX,y = coordinateY;
+        if(direction == 1 || direction == 2 || direction == 8)
+            y += 1;
+        if(direction == 4 || direction == 5 || direction == 6)
+            y -= 1;
+        if(direction == 2 || direction == 3 || direction == 4)
+            x += 1;
+        if(direction == 6 || direction == 7 || direction == 8)
+            x -= 1;
+        Unit target = field.getTarget(x,y);
+        if(target != null)
+        {
+            target.decreaseHp(damage);
+            drain();
+            if (target.getHp() == 0)
+                target.transform();
+        }
+    }
 }
