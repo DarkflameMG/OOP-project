96e482e79e80c2835450bb91ad2cea1b9916bf48
winnapat <GGgun2013@gmail.com>
2022-02-25 21:28:11 +0700

    Change CrossOrigin to * in statusCOntroller

--- carinProject/src/main/java/com/carin/carinProject/statusCOntroller.java
+++ carinProject/src/main/java/com/carin/carinProject/statusCOntroller.java
@@ -2,12 +2,14 @@ package com.carin.carinProject;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carin.carinProject.classes.Status;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(path = "api/status")
 public class statusController {
