5f9976d9a23aea66befa33e7384c77a84297ae27
Bank <deknan70@gmail.com>
2022-02-26 23:17:56 +0700

    test run game by create table

---
+++ carinProject/src/main/java/com/carin/carinProject/classes/TestGame.java
@@ -0,0 +1,37 @@
+package com.carin.carinProject.classes;
+
+import java.io.IOException;
+
+public class TestGame {
+    public static void main(String[] args) throws IOException {
+        ConfigImp.main(args);
+        FieldImp field = FieldImp.getInstance(ConfigImp.getM(),ConfigImp.getN());
+        field.placeUnit(2,3,new AntiBodyImp("ox"));
+        for(int k =0;k<3;k++)
+        {
+            for(int i=0;i<ConfigImp.getM();i++)
+            {
+                for (int j=0;j<ConfigImp.getN();j++)
+                {
+                    System.out.print("[");
+                    if(field.isEmpty(i,j) > 0)
+                    {
+                        System.out.print("-");
+                    }
+                    else
+                        System.out.print("*");
+                    System.out.print("]");
+                }
+                System.out.println();
+            }
+            try {
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            System.out.println();
+        }
+    }
+}
