084c44b2eda8d103673dc37b0f95a282448eca3e
Bank <deknan70@gmail.com>
2022-03-13 16:05:52 +0700

    test spawn Virus

--- carinProject/src/main/java/com/carin/carinProject/classes/TestGame.java
+++ carinProject/src/main/java/com/carin/carinProject/classes/TestGame.java
@@ -13,19 +13,21 @@ public class TestGame {
         geneImp.main(args);
         FieldImp field = FieldImp.getInstance(ConfigImp.getM(),ConfigImp.getN());
         MainGame game = MainGame.getInstance();
-        AntibodyFactory.getInstance().newAntibody("A1",4,0);
-        VirusFactory.getInstance().newVirus("V1",0,4);
-        AntibodyFactory.getInstance().newAntibody("A2",2,3);
-        AntibodyFactory.getInstance().newAntibody("A3",3,3);
-        VirusFactory.getInstance().newVirus("V2",3,2);
-        VirusFactory.getInstance().newVirus("V3",1,0);
+//        AntibodyFactory.getInstance().newAntibody("A1",4,0);
+//        VirusFactory.getInstance().newVirus("V1",0,4);
+//        AntibodyFactory.getInstance().newAntibody("A2",2,3);
+//        AntibodyFactory.getInstance().newAntibody("A3",3,3);
+//        VirusFactory.getInstance().newVirus("V2",3,2);
+//        VirusFactory.getInstance().newVirus("V3",1,0);
         UnitCollection allU = UnitCollection.getInstance();
 
         int v_num = field.getNum_virus();
         int a_num = field.getNum_antibody();
 
-        while((v_num !=0 && a_num !=0) || game.getGame_start() == 0)
+//        while((v_num !=0 && a_num !=0) || game.getGame_start() == 0)
+        for(int k=0;k<10;k++)
         {
+            game.VirusSpawn();
             if(game.getGame_speed() == 0)
             {
                 while (game.getGame_speed() != 0)
