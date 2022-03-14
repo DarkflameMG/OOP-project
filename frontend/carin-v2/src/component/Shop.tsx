9fe358b8677bfef734cb4cf5d2bdac9bbbb09225
ploylapas <lapasrada_k@cmu.ac.th>
2022-03-14 13:59:03 +0700

    add cost in slot shop(frontend)

--- frontend/carin-v2/src/component/Shop.tsx
+++ frontend/carin-v2/src/component/Shop.tsx
@@ -6,16 +6,19 @@ const Shop = () =>{
     return(
         <div className='shop flex flex-col'>
                     <p className="shop-text font-Righteous font-semibold">Shop</p>
-                    <p className='antibody-text slot-shop antibody-set font-Righteous '>antibody A<button >
-                        <img className='my-0.5' src={antibodyA} height={200} width={200} /></button>
+                    <p className='antibody-text slot-shop antibody-set font-Righteous '>antibody A
+                    <button ><img className='my-0.7' src={antibodyA} height={100} width={100} /></button>
+                        <p>10000</p>
                     </p>
 
-                    <p className='antibody-text slot-shop antibody-set font-Righteous'>antibody B<button >
-                        <img className='my-0.5' src={antibodyB} height={200} width={200} /></button>
+                    <p className='antibody-text slot-shop antibody-set font-Righteous'>antibody B
+                    <button ><img className='my-0.7' src={antibodyB} height={100} width={100} /></button>
+                        <p>10000</p>
                     </p>
 
-                    <p className='antibody-text slot-shop antibody-set font-Righteous'>antibody C<button>
-                        <img className='my-0.5' src={antibodyC} height={200} width={200} /></button>
+                    <p className='antibody-text slot-shop antibody-set font-Righteous'>antibody C
+                    <button><img className='my-0.7' src={antibodyC} height={100} width={100} /></button>
+                        <p>10000</p>
                     </p>
                 </div>
     )
