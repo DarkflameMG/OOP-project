5b22d0b8357858e453ec411e1652415d6f176b08
winnapat <GGgun2013@gmail.com>
2022-03-14 15:08:42 +0700

    get parameter for data in shop

--- frontend/carin-v2/src/component/Shop.tsx
+++ frontend/carin-v2/src/component/Shop.tsx
@@ -2,23 +2,23 @@ import antibodyA from '../image/antibodyA.png';
 import antibodyB from '../image/antibodyB.png';
 import antibodyC from '../image/antibodyC.png';
 import '../style/style-maingame.css'
-const Shop = () =>{
+const Shop = ({costA,costB,costC}:any) =>{
     return(
         <div className='shop flex flex-col'>
                     <p className="shop-text font-Righteous font-semibold">Shop</p>
                     <p className='antibody-text slot-shop antibody-set font-Righteous '>antibody A
                     <button ><img className='my-0.7' src={antibodyA} height={100} width={100} /></button>
-                        <p>10000</p>
+                        <p>{costA} $</p>
                     </p>
 
                     <p className='antibody-text slot-shop antibody-set font-Righteous'>antibody B
                     <button ><img className='my-0.7' src={antibodyB} height={100} width={100} /></button>
-                        <p>10000</p>
+                        <p>{costB} $</p>
                     </p>
 
                     <p className='antibody-text slot-shop antibody-set font-Righteous'>antibody C
                     <button><img className='my-0.7' src={antibodyC} height={100} width={100} /></button>
-                        <p>10000</p>
+                        <p>{costC} $</p>
                     </p>
                 </div>
     )
