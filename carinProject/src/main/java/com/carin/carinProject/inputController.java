47559d53ff7dee366b528b802968ee5ddeeb5ebb
ploylapas <lapasrada_k@cmu.ac.th>
2022-03-15 06:04:53 +0700

    add class for send frontend to backend

---
+++ carinProject/src/main/java/com/carin/carinProject/inputController.java
@@ -0,0 +1,19 @@
+package com.carin.carinProject;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "input")
+
+public class inputController {
+
+    //stateGame (play and pause)
+//    @CrossOrigin
+//    @GetMapping("/stategame")
+//    public int getStategame(){
+//
+//    }
+
+}
