85993df5da6a62506a33162c5a2e2b9b256afe17
ploylapas <lapasrada_k@cmu.ac.th>
2022-03-15 02:37:06 +0700

    set bg

--- frontend/carin-v2/src/component/Mainmemu.tsx
+++ frontend/carin-v2/src/component/Mainmemu.tsx
@@ -13,15 +13,17 @@ const Mainmenu = () =>{
     return(
         <div>
             <div className='m-auto max-w-4xl '> 
-                <div className='flex justify-center py-20 '>
-                    <img src={logo}></img>
+                <div className='flex justify-center py-20 name-game'>
+                    {/* <img src={logo}></img> */}
+                    <p>C A R I N</p>
+                    
                 </div>
-                <div className='flex justify-around space-x-10'>
-                    <img src={virusB}></img>
+                <div className='flex justify-around space-x-10 start-button'>
+                    {/* <img src={virusB}></img> */}
                     <Link to='/Maingame'>
                         <img src={startBT} ></img>
                     </Link>
-                    <img src={virus}></img>
+                    {/* <img src={virus}></img> */}
                 </div>
             </div>
             <Credit/>
