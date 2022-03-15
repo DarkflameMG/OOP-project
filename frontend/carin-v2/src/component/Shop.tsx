dfb3adfd1e59f115cc76573e7ce57e648ca3413f
winnapat <GGgun2013@gmail.com>
2022-03-15 15:23:10 +0700

    Add some on click event on shop

--- frontend/carin-v2/src/component/Shop.tsx
+++ frontend/carin-v2/src/component/Shop.tsx
@@ -2,25 +2,33 @@ import antibodyA from '../image/antibodyA.png';
 import antibodyB from '../image/antibodyB.png';
 import antibodyC from '../image/antibodyC.png';
 import '../style/style-maingame.css'
+// import {buyAntigen} from './Maingame'
+
 const Shop = ({costA,costB,costC}:any) =>{
     return(
         <div className='shop flex flex-col'>
                     <p className="shop-text font-Righteous font-semibold">Shop</p>
                     <p className='antibody-text slot-shop antibody-set font-Righteous '>antibody A
-                    <button ><img className='my-0.7' src={antibodyA} height={100} width={100} /></button>
+                    <button ><img className='my-0.7' src={antibodyA} height={100} width={100} onClick = {()=>{}}/></button>
                         <p>{costA} $</p>
                     </p>
 
                     <p className='antibody-text slot-shop antibody-set font-Righteous'>antibody B
-                    <button ><img className='my-0.7' src={antibodyB} height={100} width={100} /></button>
+                    <button ><img className='my-0.7' src={antibodyB} height={100} width={100} onClick = {()=>{}}/></button>
                         <p>{costB} $</p>
                     </p>
 
                     <p className='antibody-text slot-shop antibody-set font-Righteous'>antibody C
-                    <button><img className='my-0.7' src={antibodyC} height={100} width={100} /></button>
+                    <button><img className='my-0.7' src={antibodyC} height={100} width={100} onClick = {()=>{}}/></button>
                         <p>{costC} $</p>
                     </p>
                 </div>
     )
 }
+
+
 export default Shop
+
+function buyAntigen(arg0: string) {
+    throw new Error('Function not implemented.');
+}
