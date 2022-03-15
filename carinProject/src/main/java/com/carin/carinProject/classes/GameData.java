9a82ad5da3aa6a756e09bf64de773baf38500108
ploylapas <lapasrada_k@cmu.ac.th>
2022-03-15 14:32:20 +0700

    add credit

--- carinProject/src/main/java/com/carin/carinProject/classes/GameData.java
+++ carinProject/src/main/java/com/carin/carinProject/classes/GameData.java
@@ -5,19 +5,24 @@ import java.util.List;
 public class GameData {
     private int m;
     private int n;
+    private int credit;
     private List<Integer> cost;
     private List<Integer> posX, posY, hp, hpMax;
     private List<Integer> type;
 
-    public GameData(int m, int n,  List<Integer> cost,  List<Integer> posX, List<Integer> posY, List<Integer> hp, List<Integer> hpMax, List<Integer> type) {
+    public GameData(int m, int n,  int credit,List<Integer> cost,  List<Integer> posX, List<Integer> posY, List<Integer> hp, List<Integer> hpMax, List<Integer> type) {
         this.m = m;
         this.n = n;
+        this.credit = credit;
         this.cost = cost;
         this.posX = posX;
         this.posY = posY;
         this.hp = hp;
         this.hpMax = hpMax;
         this.type = type;
+    }
+
+    public GameData(int m, int n, Object o, Object cost, Object posX, Object posY, Object hp, Object hpMax) {
     }
 
     public int getM() {
@@ -34,6 +39,14 @@ public class GameData {
 
     public void setN(int n) {
         this.n = n;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
     }
 
     public List<Integer> getCost() {
